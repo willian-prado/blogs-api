ec454b5717838e4beadf9104f11437b1b94723cc
Willian Prado <willian_prado@hotmail.com>
2022-01-11 18:52:35 -0300

    Creates models for all tables

---
+++ models/BlogPost.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    createdAt: { field: 'published', type: DataTypes.STRING },
+    updatedAt: { field: 'updated', type: DataTypes.STRING },
+  },
+  {
+    timestamps: false,
+    createdAt: 'published',
+    updatedAt: 'updated',
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'user' });
+  };
+
+  return BlogPost;
+};
